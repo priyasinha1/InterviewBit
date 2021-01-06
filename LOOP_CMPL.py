a093207c6d58474e572ecd1851bcda88e14d421e
Priya Sinha <64432440+priyasinha1@users.noreply.github.com>
2021-01-06 11:38:15 +0530

    Update LOOP_CMPL.py
    Given three points are: (0, 0), (1, 1) and (1, 2).
     It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move from (1, 1) to (1, 2).

--- LOOP_CMPL.py
+++ LOOP_CMPL.py
@@ -1,1 +1,30 @@
+#Input Format
+#Given two integer arrays A and B, where A[i] is x coordinate and B[i] is y coordinate of ith point respectively.
+#Output Format
+#Return an Integer, i.e minimum number of steps.
+#Example Input
+#Input 1:
 
+# A = [0, 1, 1]
+# B = [0, 1, 2]
+
+
+#Example Output
+#Output 1: 2
+
+
+
+class Solution:
+    # @param A : list of integers
+    # @param B : list of integers
+    # @return an integer
+    def coverPoints(self, A, B):
+        pre_x, pre_y, total = A[0], B[0], 0
+        for cur_x, cur_y in zip(A, B):
+            dx, dy = abs(cur_x - pre_x), abs(cur_y - pre_y)
+            if dx < dy:
+                total += dy
+            else:
+                total += dx
+            pre_x, pre_y = cur_x, cur_y
+        return total
