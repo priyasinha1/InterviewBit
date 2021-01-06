7f43c51721abbed021768845b336da295e8e3b48
Priya Sinha <64432440+priyasinha1@users.noreply.github.com>
2021-01-06 12:01:34 +0530

    Update largest_number.py

--- largest_number.py
+++ largest_number.py
@@ -5,10 +5,8 @@ class Solution:
 	# @return a strings
 	def largestNumber(self, A):
 	    
-	    # Convert to string once, for proper comparison a+b vs b+a
 	    A = map(str, A)
 	    key = cmp_to_key(lambda a,b: 1 if a+b >= b+a else -1)
 	    res = ''.join(sorted(A, key= key, reverse=True))
-	    # Must left trim 0, apparently
 	    res = res.lstrip('0')
 	    return res if res else '0'
